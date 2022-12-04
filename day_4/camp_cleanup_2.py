2f38aace7dd95155e26ed454710f0e6c8fb55f76
Grzegorz Marian Socha <01158907@pw.edu.pl>
2022-12-04 11:48:53 +0100

    solved day_4

---
+++ day_4/camp_cleanup_2.py
@@ -0,0 +1,35 @@
+
+file_name = "input.txt"
+
+
+def read_from_file(path: str) -> list:
+    try:
+        with open(path, 'r') as file_handle:
+            items = []
+            for line in file_handle:
+                first, second = line.rstrip().split(',')
+                items.append((first.split('-'), second.split('-')))
+            return items
+    except FileNotFoundError:
+        print(f'File {path} not found.')
+
+
+def get_sum_of_overlaps(input_list: list) -> int:
+    sum = 0
+    for line in input_list:
+        first, second = line
+        if int(second[0]) in range(int(first[0]), int(first[1]) + 1):
+            sum += 1
+        elif int(second[1]) in range(int(first[0]), int(first[1]) + 1):
+            sum += 1
+        elif int(first[0]) in range(int(second[0]), int(second[1]) + 1):
+            sum += 1
+        elif int(first[1]) in range(int(second[0]), int(second[1]) + 1):
+            sum += 1
+    return sum
+
+
+if __name__ == "__main__":
+    input = read_from_file(file_name)
+    if input is not None:
+        print(get_sum_of_overlaps(input))
